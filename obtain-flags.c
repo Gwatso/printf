5f518b955354aa13a016d870b47737def358235d
Tapiwanashe92 <tapiwanashembk@gmail.com>
2023-04-25 01:02:10 -0700

    a function that calculates active flags

---
+++ obtain-flags.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * get_flags - calculates the active flags
+ * @format: formatted string to print the arguments
+ * @a: take a parameter.
+ * Return: Flags
+ */
+int get_flags(const char *format, int *a)
+{
+	/* - + 0 # ' ' */
+	/* 1 2 4 8  16 */
+	int q, curr_a;
+	int flags = 0;
+	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
+	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+
+	for (curr_a = *a + 1; format[curr_a] != '\0'; curr_a++)
+	{
+		for (q = 0; FLAGS_CH[q] != '\0'; q++)
+			if (format[curr_a] == FLAGS_CH[q])
+			{
+				flags |= FLAGS_ARR[q];
+				break;
+			}
+
+		if (FLAGS_CH[q] == 0)
+			break;
+	}
+
+	*a = curr_a - 1;
+
+	return (flags);
+}
