c2625348a8edef1a5fc26de833644a69abdf4fe1
Tapiwanashe92 <tapiwanashembk@gmail.com>
2023-04-26 01:17:24 -0700

    updated file

--- obtain-flags.c
+++ obtain-flags.c
@@ -1,34 +1,34 @@
 #include "main.h"
 
 /**
- * get_flags - calculates the active flags
+ * obtain_flags - calculates the active flags
  * @format: formatted string to print the arguments
  * @q: take a parameter.
  * Return: Flags
  */
-int get_flags(const char *format, int *ptrq)
+int obtain_flags(const char *format, int *q)
 {
 	/* - + 0 # ' ' */
 	/* 1 2 4 8  16 */
-	int q, curr_q;
+	int a, curr_q;
 	int flags = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
 	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
-	for (curr_q = *ptrq + 1; format[curr_q] != '\0'; curr_q++)
+	for (curr_q = *q + 1; format[curr_q] != '\0'; curr_q++)
 	{
-		for (q = 0; FLAGS_CH[q] != '\0'; q++)
-			if (format[curr_q] == FLAGS_CH[q])
+		for (a = 0; FLAGS_CH[a] != '\0'; a++)
+			if (format[curr_q] == FLAGS_CH[a])
 			{
-				flags |= FLAGS_ARR[q];
+				flags |= FLAGS_ARR[a];
 				break;
 			}
 
-		if (FLAGS_CH[q] == 0)
+		if (FLAGS_CH[a] == 0)
 			break;
 	}
 
-	*ptrq = curr_q - 1;
+	*q = curr_q - 1;
 
 	return (flags);
 }
